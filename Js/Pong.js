5f37841b5a31b77b1166e8dbf596c150c1ecbc2f
oskadoskaposka <alepaduanlima@gmail.com>
2020-07-30 00:33:56 -0300

    Refatoração código com arrow functions e if ternario

--- Js/Pong.js
+++ Js/Pong.js
@@ -5,8 +5,14 @@ let diametroBolinha = 15
 let raioBolinha = diametroBolinha / 2
 
 // Variáveis da velocidade da bolinha
-let velocidadeXBolinha = 6
-let velocidadeYBolinha = 6
+let velocidadeXBolinha = 3
+let velocidadeYBolinha = 3
+
+// Variáveis da Raquete
+let xRaquete = 5
+let yRaquete = 150
+let comprimentoRaquete = 10
+let alturaRaquete = 100
 
 // Criar quadro
 function setup() {
@@ -18,24 +24,33 @@ function draw() {
   background(0);
   criarBolinha()
   moverBolinha()
-  verificaColisao ()
+  verificaColisao()
+  criarRaquete()
+  movimentarMinhaRaquete()
 }
 
 // Criar a bolinha
-function criarBolinha () {
-  circle (xBolinha, yBolinha, diametroBolinha)
-}
+criarBolinha = () => circle(xBolinha, yBolinha, diametroBolinha)
 
 // Mover a bolinha dentro do espaço definido
-function moverBolinha () {
+moverBolinha = () => {
   xBolinha += velocidadeXBolinha
   yBolinha += velocidadeYBolinha
 }
 
-// Verifica a colisão com as bordas do espaço
-function verificaColisao () {
-  if (xBolinha > width -raioBolinha || xBolinha < 0 + raioBolinha) {
-  velocidadeXBolinha = velocidadeXBolinha *-1}
-  if (yBolinha > height -raioBolinha || yBolinha < 0 + raioBolinha) {
-  velocidadeYBolinha = velocidadeYBolinha *-1}
+// Verifica a colisão da bolinha com as bordas do espaço
+verificaColisao = () => {
+  if (xBolinha > width - raioBolinha || xBolinha < 0 + raioBolinha){
+    velocidadeXBolinha *= -1}
+  if (yBolinha > height - raioBolinha || yBolinha < 0 + raioBolinha){
+    velocidadeYBolinha *= -1}
+}
+
+// Criar a raquete
+criarRaquete = () => rect(xRaquete, yRaquete, comprimentoRaquete,   alturaRaquete)
+
+// Movimentar a Raquete
+movimentarMinhaRaquete = () => {
+  keyIsDown(UP_ARROW) ? yRaquete -= 10 : ""
+  keyIsDown(DOWN_ARROW) ? yRaquete += 10 : ""
 }
